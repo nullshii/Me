cfdcb5a4761973f22e5098b4975dd4d2cb77700e
Ray <60722967+PureFoxCore@users.noreply.github.com>
2022-08-30 03:48:23 +0400

    Centered items (for mobile view (for @blakewonka))

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className='bg-slate-600 flex items-center justify-center flex-col h-screen'>
-      <h1 className='text-6xl text-white'>Here begins my portfolio</h1>
-      <h1 className='text-sm text-white pt-5'>just give me some time to build it ;)</h1>
+      <h1 className='text-6xl text-center text-white'>Here begins my portfolio</h1>
+      <h1 className='text-sm text-center text-white pt-5'>just give me some time to build it ;)</h1>
     </div>
   );
 }
